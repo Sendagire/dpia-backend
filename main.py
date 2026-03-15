1434da84d388729430ace25614a8de70d3954209
Sendagire <167121581+Sendagire@users.noreply.github.com>
2026-03-15 12:40:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], # Allows any website to connect
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
